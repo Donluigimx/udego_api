86a85f63dda62c548190b19b8220663c374a4a6f
Luis Angel Iñiguez Hernández <luigi.lahi@gmail.com>
2017-03-06 12:12:55 -0600

    chat server fixed

--- SocketServer/app.js
+++ SocketServer/app.js
@@ -1,3 +1,4 @@
+var compression = require('compression');
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -17,6 +18,7 @@ var index = require('./routes/index')(io);
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(compression());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
